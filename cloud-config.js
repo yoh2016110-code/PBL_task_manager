1ccea521a10af35378f6f53d005d7200a8ac76f2
Haruto Ishigaki <47k99ksf97@privaterelay.appleid.com>
2026-07-09 07:08:44 +0900

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -1,4 +1,1 @@
-window.GTJ_SUPABASE = {
-  url: "https://cwaoznqmtyvvlarnnovo.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3YW96bnFtdHl2dmxhcm5ub3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MDY3OTAsImV4cCI6MjA5ODQ4Mjc5MH0.ydge5KSBrNOaxuTjEHqqre-3ziWQAuL57Yn5NoSJ1Rg"
-};
+window.GTJ_SUPABASE = null;
