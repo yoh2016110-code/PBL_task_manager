9974a6c5c2251270eec0bb6e476f7464e5952818
yoh2016110-code <u828089j@ecs.osaka-u.ac.jp>
2026-07-16 11:40:23 +0900

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -3,4 +3,4 @@ window.GTJ_SUPABASE = {
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3YW96bnFtdHl2dmxhcm5ub3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MDY3OTAsImV4cCI6MjA5ODQ4Mjc5MH0.ydge5KSBrNOaxuTjEHqqre-3ziWQAuL57Yn5NoSJ1Rg"
 };
 
-window.GTJ_AI_PROXY_URL = "https://pbltaskmanager.47k99ksf97.workers.dev/";
+window.GTJ_AI_PROXY_URL = "https://pbltaskmanager.yoh2016110-code.workers.dev/";
